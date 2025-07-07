999f3669494b19a7fb25e931278b7150537d2de9
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2025-07-07 12:27:23 +0200

    test: change delete step

--- internal/controllers/compositiondefinitions/compositiondefinitions_test.go
+++ internal/controllers/compositiondefinitions/compositiondefinitions_test.go
@@ -317,14 +317,11 @@ func TestCreate(t *testing.T) {
 
 		res := v1alpha1.CompositionDefinition{}
 
-		err = decoder.DecodeFile(
-			os.DirFS(filepath.Join(testdataPath, "compositiondefinitions_test")), testFileName,
-			&res,
-			decoder.MutateNamespace(resource_ns),
-		)
+		err = r.Get(ctx, "fireworks-app-2", resource_ns, &res)
 		if err != nil {
 			t.Fatal(err)
 		}
+
 		// Delete CompositionDefinition
 		err = r.Delete(ctx, &res)
 		if err != nil {
