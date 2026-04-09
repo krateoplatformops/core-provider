f6260082b30ae792ffe3f8caa0ae20748fad9b54
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2026-04-09 13:59:11 +0200

    fix: update build script for consistency and remove unused local build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-KO_DOCKER_REPO=kind.local KIND_CLUSTER_NAME=krateo-v2 ko build --base-import-paths ..
-printf '\n\nList of current docker images loaded in KinD:\n'
-
-kubectl get nodes kind-control-plane -o json \
-    | jq -r '.status.images[] | " - " + .names[-1]'
+KO_DOCKER_REPO=kind.local KIND_CLUSTER_NAME=krateo-quickstart ko build --base-import-paths .
